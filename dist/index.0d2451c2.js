8e864e496671d3b628e0c4e4fa7e44eb6fcb8502
Dima Pesotskiy <d.pesotzky@yandex.by>
2024-09-02 12:40:20 +0300

    duild project

---
+++ dist/index.0d2451c2.js
@@ -0,0 +1,3 @@
+new Swiper(".image-slider",{pagination:{el:".swiper-pagination",clickable:!0}});let e=document.querySelector("#burger"),i=document.querySelector(".header-active-box__menu-burger");e.addEventListener("click",()=>{i.classList.toggle("activ")});//# sourceMappingURL=index.0d2451c2.js.map
+
+//# sourceMappingURL=index.0d2451c2.js.map
